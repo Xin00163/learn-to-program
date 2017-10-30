2c95843a1087ed6ed98ee8b4ffdb13cd7f504597
Xin Wang <wangxin00163@yahoo.co.uk>
2017-10-30 20:04:49 +0000

    Chapter 2 & 5

---
+++ chap02/ex1.rb
@@ -0,0 +1,11 @@
+#hours in a year
+puts 24*365
+
+#minutes in a decade
+puts 60*24*(365*10+2)
+
+#my age in seconds
+puts 29*365*24*60*60
+
+#how old is the author
+puts 1160000000/60/60/24/365
---
+++ chap05/ex2.rb
@@ -0,0 +1,3 @@
+puts "What's your favorite number?"
+number = gets.chomp
+puts "#{number.to_i + 1} is a bigger and better favorite number."

aea44ff95e9cb2ad4a1338f82a495eb679cec77b
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-05 12:35:18 +0000

    exercises for chap10

--- chap10/ex4.rb
+++ chap10/ex4.rb
@@ -1,10 +1,61 @@
-number = 9999
-while number > 1
-  puts "#{number} bottles of beer on the wall, #{number} bottles of beer. "
-  number -= 1
-  puts "Take one down and pass it around, #{number} bottles of beer on the wall."
+def english_number number
+  num_string = ''
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
+  left = number
+  write = left/100
+  left = left - write*100
+
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if (write == 1) and (left > 0)
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write - 1]
+    end
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write - 1]
+  end
+
+  num_string
 end
 
-puts "1 bottle of beer on the wall, 1 bottle of beer."
+number_at_start = 40
+number_now = number_at_start
+while number_now > 2
+  puts english_number(number_now).capitalize + " bottles of beer on the wall, " + english_number(number_now) + " bottles of beer!"
+  number_now = number_now - 1
+  puts "Take one down and pass it around, " + english_number(number_now) + " bottles of beer on the wall."
+end
+
+puts "Two bottles of beer on the wall, two bottles of beer!"
+puts "Take one down, pass it around, one bottle of beer on the wall!"
+puts "One bottle of beer on the wall, one bottle of beer."
 puts "Take one down and pass it around, no more bottles of beer on the wall."
 puts "No more bottles of beer on the wall, no more bottles of beer."
